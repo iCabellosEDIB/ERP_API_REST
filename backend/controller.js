c844f9725bf1290067dec70054bd4c8ff83b51f6
Ivan <i.cabellos@id.uib.eu>
2024-12-02 16:58:23 +0100

    maped employees

--- backend/controller.js
+++ backend/controller.js
@@ -1,25 +1,34 @@
 // controller.js
 
-// URL to API (reemplaza con la URL de tu API)
-var url = 'http://localhost:3000/API/employees/json';
+// URL to API
+const server_url = 'http://localhost:3000';
+let get_employees_URL = '/API/employees/json';
+// UPDATE URL TO CALL SERVICE
+let url = server_url.concat(get_employees_URL);
 
 // Fetch to API Users
+
 fetch(url)
 .then(function(response) {
     if (!response.ok) {
-      throw new Error(response.ok);
+        throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    console.log(response);
-    console.log(response.json);
     return response.json();
 })
 .then(function(data) {
-    // CALL FUNCTIONS TO PROCESS DATA HERE
+    let employees = data.map(employee => ({
+        id: employee.id,
+        name: employee.name,
+        country: employee.born_in_country,
+        gender: employee.gender,
+        isAdmin: employee.isAdmin,
+        createdAt: new Date(employee.created).toLocaleDateString()
+    }));
+    // CALL PRINT FUNCTIONS HERE
 })
 .catch(function(error) {
     console.error('Error:', error);
 });
-
 
 // TEST 1
 
